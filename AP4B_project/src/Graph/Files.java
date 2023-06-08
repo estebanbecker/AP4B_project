9525a1e70b648b4600b2cc07e04f9eb1108b0ec0
Elise Albrecht <vitally.tally67@gmail.com>
2023-06-08 18:04:20 +0200

    Adding comments

--- AP4B_project/src/Graph/Files.java
+++ AP4B_project/src/Graph/Files.java
@@ -7,6 +7,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * This class is used to manage files, including reading and writing.
+ * The processed format is .txt, so that information can be read in plain text.
+ * The data is stored in arrays of size of elements funded ( that we will write or read).
+ */
+
 public class Files {
     private String filePath;
     private String flag1 = "***";
@@ -25,17 +31,24 @@ public class Files {
         this.filePath = filePath;
     }
 
+
+
+    /** Get information from a file
+     * The file has a specific formatting detailed in the report
+     * @return a graph
+     **/
     public Graph readFile() {
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));   //Create a reader with the library BufferedReader
             String line;
 
             line = reader.readLine();
-            if (line.equals(flag1)) { //Premier flag d'entrée
+            if (line.equals(flag1)) {
 
                 line = reader.readLine();
-                do {
+
+                do {                                            //The first bloc of information are the nodes
                     if (!line.equals(flag2)) {
                         id.add(Integer.parseInt(line));
                         line = reader.readLine();
@@ -56,7 +69,7 @@ public class Files {
                 } while (!line.equals(flag1));
 
                 line = reader.readLine();
-                do {
+                do {                                           //The second bloc has the information of the edges
                     if (!line.equals(flag2)) {
                         id_node_from.add(Integer.parseInt(line));
                         line = reader.readLine();
@@ -81,7 +94,7 @@ public class Files {
             e.printStackTrace();
         }
 
-        Integer[] id_array = id.toArray(new Integer[id.size()]);
+        Integer[] id_array = id.toArray(new Integer[id.size()]);        //Converting from ArrayList to simple arrays (of the proper type)
         Float[] x_array = x.toArray(new Float[x.size()]);
         Float[] y_array = y.toArray(new Float[y.size()]);
 
@@ -89,43 +102,46 @@ public class Files {
         Integer[] id_node_to_array = id_node_to.toArray(new Integer[id_node_to.size()]);
         String[] label_array = label.toArray(new String[label.size()]);
 
-        Graph read_graph = new Graph(id_array,x_array,y_array);
+        Graph read_graph = new Graph(id_array,x_array,y_array);                     //Creating a new graph with the values read
         read_graph.createEdges(id_node_from_array,id_node_to_array,label_array);
 
         return read_graph;
     }
 
+    /** Write a file with the current graph
+     * @param graph
+     */
     public void writeFile(Graph graph) {
-        NodeData node = graph.getNodesData();
+        NodeData node = graph.getNodesData();           //Retrieve information of the current graph
         EdgeData edge = graph.getEdgesData();
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));    //Create a writer with the library BufferedWriter
 
-            writer.write(flag1);
+            writer.write(flag1);            //Adding the first flag
             writer.newLine();
 
-            for (int i = 0; i < node.getlength(); i++) {
+            for (int i = 0; i < node.getlength(); i++) {                //Writing information about the nodes
                 writer.write(String.valueOf(node.ids[i]));
                 writer.newLine();
                 writer.write(String.valueOf(node.xValues[i]));
                 writer.newLine();
                 writer.write(String.valueOf(node.yValues[i]));
                 writer.newLine();
-                writer.write(flag2);
+                writer.write(flag2);                                    //Adding the other flag when it's done
                 writer.newLine();
             }
 
-            writer.write(flag1);
+            writer.write(flag1);            //Separating blocs
             writer.newLine();
 
-            for (int i = 0; i < edge.node_from_ids.length; i++) {
+            for (int i = 0; i < edge.node_from_ids.length; i++) {       //Writing information about the edges
                 writer.write(String.valueOf(edge.node_from_ids[i]));
                 writer.newLine();
                 writer.write(String.valueOf(edge.node_to_ids[i]));
                 writer.newLine();
                 writer.write(edge.labels[i]);
                 writer.newLine();
-                writer.write(flag2);
+                writer.write(flag2);                                    //Adding the other flag when it's done
                 writer.newLine();
             }
 
@@ -136,5 +152,4 @@ public class Files {
             e.printStackTrace();
         }
     }
-
-}
+};
